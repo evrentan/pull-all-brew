5e58eb13f7f9eb0505fad45823a35b6932cce534
Evren Tan <evrentan@gmail.com>
2025-06-30 21:47:21 +0300

    feat(pull-all): update sha256

--- Formula/pull-all.rb
+++ Formula/pull-all.rb
@@ -2,7 +2,7 @@ class PullAll < Formula
     desc "Bash script to pull all git repos in a directory"
     homepage "https://github.com/evrentan/pull-all"
     url "https://github.com/evrentan/pull-all/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "5fdca5d215781ec9f3474fc16cde8b5094ccb9023817cdcd5974858c39e0f77c"
+    sha256 "a5b6ebfbc19e6560c42c2a95a39e9a84d6366839283987b33384946366790d35"
     license "MIT"
   
     def install

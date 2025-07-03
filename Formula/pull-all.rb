904829b5cfd06d3ce1a2c202a25ce99e73e5f2be
Evren Tan <evrentan@gmail.com>
2025-07-03 23:05:48 +0300

    feat(pull-all): update pull-all tag to v1.1.0

--- Formula/pull-all.rb
+++ Formula/pull-all.rb
@@ -1,8 +1,8 @@
 class PullAll < Formula
     desc "Bash script to pull all git repos in a directory"
     homepage "https://github.com/evrentan/pull-all"
-    url "https://github.com/evrentan/pull-all/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "a5b6ebfbc19e6560c42c2a95a39e9a84d6366839283987b33384946366790d35"
+    url "https://github.com/evrentan/pull-all/archive/refs/tags/v1.1.0.tar.gz"
+    sha256 "4b741bd25b22ab6e0b6985257fdcc45d7769cf0b6d332812c512aff86c0796d0"
     license "MIT"
   
     def install
